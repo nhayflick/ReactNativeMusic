c5011899936d49f95734742e3d77fb793afb778a
Nathan Hayflick <nathan@roqbot.com>
2015-05-22 00:24:45 -0400

    Blog post 2: Live data and navigation

--- index.ios.js
+++ index.ios.js
@@ -12,25 +12,13 @@ var {
   NavigatorIOS,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
   View,
 } = React;
-
-
-var mockedData = [{"genre":"Deep House",
-  title: "Coffee (Manhattoes Remix)",
-  user:  {
-    username: "manhattoes"
-  },
-  artwork_url: "https://i1.sndcdn.com/artworks-000107004661-v4xg0d-large.jpg",
-  stream_url: "https://api.soundcloud.com/tracks/191554493/stream"
-},{genre: "Bass",
-  title: "Threadsafe",
-  user: {
-    username: "manhattoes"
-  },
-  artwork_url: "https://i1.sndcdn.com/artworks-000104588970-5kyi09-large.jpg",
-  stream_url: "https://api.soundcloud.com/tracks/187885871/stream"
-}];
+var TimerMixin = require('react-timer-mixin');
+var SOUNDCLOUD_CLIENT_ID = 'ff3108ddadaeeea1c2cd56d0b3617255';
 
 var ReactNativeMusic = React.createClass({
   getInitialState: function () {
@@ -39,20 +27,14 @@ var ReactNativeMusic = React.createClass({
     }
   },
   render: function() {
-   //  return (
-   //    <View style={styles.appContainer}>
-    //     <NavigatorIOS style={styles.navContainer}
-      //     barTintColor='blue' 
-      //     titleTextColor='white' 
-      //     initialRoute={{
-      //     title: 'SoundCloud Native',
-      //      component: BrowseTracksView}} />
-      //    <NowPlayingFooter />
-      // </View>
-   //  );
     return (
       <View style={styles.appContainer}>
-        <BrowseTracksView />
+        <NavigatorIOS style={styles.navContainer}
+          barTintColor='#F5FCFF' 
+          initialRoute={{
+            title: 'ReactNativeMusic',
+            component: BrowseTracksView
+          }} />
         <NowPlayingFooterView nowPlaying={this.props.nowPlaying}/>
       </View>
     );
@@ -61,33 +43,60 @@ var ReactNativeMusic = React.createClass({
 
 // List View For Browsing Songs
 var BrowseTracksView = React.createClass({
+  mixins: [TimerMixin],
+  timeoutID: (null: any),
   getInitialState: function () {
     return {
       dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
+        rowHasChanged: (row1, row2) => row1 !== row2
       })
     };
   },
   componentDidMount: function() {
     this.fetchData();
   },
-  fetchData: function () {
-    // Return mocked data
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(mockedData)
-    });
+  fetchData: function (query) {
+    var queryString = '';
+    if (query) {
+      queryString = '&q=' + query
+    }
+    // Return live data
+    fetch(this.fetchEndpoint + queryString)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData)
+        });
+      }).catch((error) => {
+        console.warn(error);
+      })
+      .done();
+  },
+  fetchEndpoint: 'http://api.soundcloud.com/tracks.json?client_id=' + SOUNDCLOUD_CLIENT_ID,
+  onSearchChange: function (event) {
+    var q = event.nativeEvent.text.toLowerCase();
+    this.clearTimeout(this.timeoutID);
+    this.timeoutID = this.setTimeout(() => this.fetchData(q), 100);
   },
   render: function () {
     return (
       <ListView 
         dataSource={this.state.dataSource}
         renderRow={this.renderTrack}
+        renderHeader={this.renderSearchBar}
         style={styles.listView}/>
     );
   },
+  renderSearchBar: function () {
+    return (
+      <View style={styles.searchCell}>
+        <TextInput onChange={this.onSearchChange} placeholder={'Search Here'} style={styles.searchContainer}/>
+      </View>
+    )
+  },
   renderTrack: function (track) {
     return (
-      <TrackCell track={track}/>
+      <TrackCell navigator={this.props.navigator} track={track}/>
     );
   }
 });
@@ -115,19 +124,48 @@ var NowPlayingFooterView = React.createClass({
 var TrackCell = React.createClass({
   render: function () {
     return (
-      <View style={styles.trackCell}>
-        <Image
-          source={{uri: this.props.track.artwork_url}}
-          style={styles.thumbnail} />
-        <View style={styles.rightContainer}>
-          <Text style={styles.trackTitle}>{this.props.track.title}</Text>
-          <Text style={styles.trackArtist}>{this.props.track.user.username}</Text>
+      <TouchableOpacity onPress={() => this.selectTrack(this.props.track)} style={styles.rightContainer}>
+        <View style={styles.trackCell}>
+          <Image
+            source={{uri: this.props.track.artwork_url}}
+            style={styles.thumbnail} />
+          <View style={styles.rightContainer}>
+            <Text style={styles.trackTitle}>{this.props.track.title}</Text>
+            <Text style={styles.trackArtist}>{this.props.track.user.username}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  },
+  selectTrack: function (track) {
+    this.props.navigator.push({
+      title: track.title,
+      component: TrackScreen,
+      passProps: {track}
+    });
+  }
+});
+
+var TrackScreen = React.createClass({
+  render: function () {
+    var largeImageUrl = this.props.track.artwork_url ? this.props.track.artwork_url.replace('-large', '-t300x300') : '';
+    return (
+      <View style={styles.trackScreen}>
+       <Image
+          style={styles.largeArtwork}
+          source={{uri: largeImageUrl}}>
+        </Image>
+        <Text style={styles.trackTitle}>{this.props.track.title}</Text>
+        <Text style={styles.trackArtist}>{this.props.track.user.username}</Text>
+        <View style={styles.buttonRow}>
+          <TouchableHighlight style={styles.playButton}>
+            <Text style={styles.playButtonText}>Play Track</Text>
+          </TouchableHighlight>
         </View>
       </View>
     );
   }
 });
-
 
 var styles = StyleSheet.create({
   appContainer: {
@@ -135,19 +173,53 @@ var styles = StyleSheet.create({
     paddingTop: 20,
     backgroundColor: '#F5FCFF'
   },
-  navContainer: {
-    flex: 1
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+  },
+  largeArtwork: {
+    width: 300,
+    height: 300
   },
   listView: {
      backgroundColor: '#F5FCFF',
+  },
+  navContainer: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
   },
   nowPlayingFooter: {
     flex: 0,
     borderTopWidth: .5
   },
-  thumbnail: {
-    width: 50,
-    height: 50,
+  playButton: {
+    backgroundColor: '#4472B9',
+    margin: 4,
+    padding: 4,
+    borderRadius: 4,
+    flex: 1
+  },
+  playButtonText: {
+    color: 'white',
+    fontSize: 20
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  searchCell: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  searchContainer: {
+    height: 40,
+    width: 100,
+    flex: 1,
+    margin: 4,
+    padding: 4,
+    borderColor: 'gray',
+    color: 'black',
+    borderWidth: 1
   },
   trackCell: {
     flex: 1,
@@ -160,6 +232,13 @@ var styles = StyleSheet.create({
     borderBottomWidth: .5,
     borderColor: 'lightgray'
   },
+  trackScreen: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
   trackTitle: {
     fontSize: 20,
     marginBottom: 8,
@@ -170,8 +249,9 @@ var styles = StyleSheet.create({
     marginBottom: 6,
     textAlign: 'center',
   },
-  rightContainer: {
-    flex: 1,
+  thumbnail: {
+    width: 50,
+    height: 50,
   }
 });
 
